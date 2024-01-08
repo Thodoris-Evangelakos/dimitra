1d283236dda4a1ed0be406a74f9f5fdfcb6a8013
Thodoris-Evangelakos <tedevangel@yandex.com>
2024-01-08 02:06:57 +0200

    comments kai teleutaies pinelies. SENTINEL

--- main.py
+++ main.py
@@ -1,80 +1,85 @@
-import csv, sys, ast, os
-import calendar
-from Perifereia import Perifereia
-from Seismos import Seismos
+import csv, calendar, ast, glob
 import shapely.geometry as sg
 from shapely.geometry import Polygon, MultiPolygon
+import matplotlib.pyplot as plt
 
-perifereies_file = "perifereies.csv"
-seismoi_path = "seismoi.dat"
-peloponnhsos_fid = "d7f50467-e5ef-49ac-a7ce-15df3e2ed738.9"
-perifereies_path = "data/regions"
+from Perifereia import Perifereia
+from Seismos import Seismos
+
+
+
+PERIFEREIES_FILE = "perifereies.csv"
+SEISMOI_PATH = "seismoi.dat"
+PERIFEREIES_PATH = "data/regions"
 regions = ['Π. ΑΝΑΤΟΛΙΚΗΣ ΜΑΚΕΔΟΝΙΑΣ - ΘΡΑΚΗΣ', 'Π. ΚΕΝΤΡΙΚΗΣ ΜΑΚΕΔΟΝΙΑΣ', 'Π. ΔΥΤΙΚΗΣ ΜΑΚΕΔΟΝΙΑΣ', 'Π. ΗΠΕΙΡΟΥ', 'Π. ΘΕΣΣΑΛΙΑΣ', 'Π. ΒΟΡΕΙΟΥ ΑΙΓΑΙΟΥ', 'Π. ΝΟΤΙΟΥ ΑΙΓΑΙΟΥ', 'Π. ΣΤΕΡΕΑΣ ΕΛΛΑΔΑΣ', 'Π. ΔΥΤΙΚΗΣ ΕΛΛΑΔΑΣ', 'Π. ΠΕΛΟΠΟΝΝΗΣΟΥ', 'Π. ΙΟΝΙΩΝ ΝΗΣΩΝ', 'Π. ΚΡΗΤΗΣ', 'Π. ΑΤΤΙΚΗΣ']
+DESIRED_INDICES = [4, 12]
+OUTPUT_PATH = "output/"
 csv.field_size_limit(2**31-1)
 
+# ena karo constants, enas theos kserei an xreiazontan kai ola
+
+
+# lambda func gia na kanoume to asxhmo timestamp wraio mhna
 get_month = lambda timestamp: calendar.month_name[int(timestamp[:2])]
 
-# read sumn
 
+# operations gia na kanw dedomena twn csv objects
 def findPolygons(polygons_data):
-    if not polygons_data.strip():  # Check if the input string is empty or contains only whitespace
+    if not polygons_data.strip():  # prostasia apo empty string input, w/e
         return []
     polygons = []
-    # Remove double quotes
+    # peta double quotes
     polygons_data = polygons_data.replace('"', '')
-    # Remove 'MULTIPOLYGON ((' and '))' from the start and end, and split by ')), ((' to get a list of polygons
+    # katharizoume 'MULTIPOLYGON ((' kai '))' apo arxh kai telos, spame to string me delimeters ta ')), ((' gia na skasei lista apo polygons
     polygons_data = polygons_data.replace('MULTIPOLYGON (((', '').rstrip('))').split(')), ((')
     for polygon_data in polygons_data:
-        # Split by ', ' to get a list of points
+        # delimeter to komma gia na paroume list of points
         points = polygon_data.split(', ')
-        # For each point, split by space and convert the numbers to float
+        # kathe point anti gia komma anamesa se lat/lon exei keno, poly eksypno /s
         points = [tuple(map(float, point.split())) for point in points]
         polygons.append(tuple(points))
     return polygons
 
+
+# apo to perifereies.csv pou phrame apo to kratos ftiaxnw Perifereia objects. gia kapoio logo to encoding='utf-8' einai aparaithto sta windows
+# alliws den mporei na diavasei to arxeio
 def loadPerifereies() -> list:
     perifereies = []
-    with open(perifereies_file, 'r', encoding='utf-8') as f:
+    with open(PERIFEREIES_FILE, 'r', encoding='utf-8') as f:
         reader = csv.reader(f)
-        next(reader)  # Skip the header
+        next(reader)  # skip to header
         for row in reader:
             FID, per, polygons_data = row
-            polygons = findPolygons(polygons_data)  # Pass the polygon data to findPolygons
+            polygons = findPolygons(polygons_data)  # dedicated function gia na kanei untagle ta multipolygon data, h kalyterh epilogh mou olo to 2024
             perifereies.append(Perifereia(FID, per, polygons))
-        #print([perifereia.per for perifereia in perifereies])
     return perifereies
 
+# kathe perifereia kai ena csv me to polygons ths
 def serializePerifereies(perifereies):
     for i, perifereia in enumerate(perifereies):
         perifereia.file(i)
 
+ 
+# pairnw ta region{index}.csv files kai ta kanw MultiPolygon objects, poly pio grhgora sth xrhsh apto na kanw iterate over every polygon
 def readPolygonsFromFile(index):
-    path = f"{perifereies_path}/region{index}.csv"
+    path = f"{PERIFEREIES_PATH}/region{index}.csv"
     polygons = []
     with open(path, 'r') as file:
         for line in file:
-            # Remove whitespace and convert string representation of tuples to actual tuples
+            # full voithitiko pou mporw na kanw interpret to ((float, float), (float,float)) ws tuple
             polygon_coordinates = ast.literal_eval(line.strip())
-            # Create a Polygon object and append it to the list
+            # wraio class, den xreiasthke na to kanw egw
             polygons.append(Polygon(polygon_coordinates))
-    # Create a MultiPolygon object from the list of Polygon objects
+    # synthesi multipolygon, 2h kalyterh epilogh mou olo to 2024
     multipolygon = MultiPolygon(polygons)
     return multipolygon
 
+
 def openSeismoiFile():
-    # too bulky, can be optimized but w.e
-    with open(seismoi_path, 'r') as f:
-        data = []
-        for i, line in enumerate(f):
-            if i > 17 and i < 11425:
-                data.append(tuple(line.split(" ")))
-        ref_data = []
-        for lst in data:
-            ref_data.append([entry for entry in lst if entry != ''])
-        seismoi = []
-        for item in ref_data:
-            year, dates, lat, lon, dep, M = item
-            seismoi.append(Seismos(year, dates, float(lat), float(lon), dep, M))
+    with open(SEISMOI_PATH, 'r') as f:
+        data = [tuple(line.split(" ")) for i, line in enumerate(f) if 17 < i < 11425]
+        ref_data = [[entry for entry in lst if entry != ''] for lst in data]
+        seismoi = [Seismos(year, dates, float(lat), float(lon), dep, M) for year, dates, lat, lon, dep, M in ref_data]
         return seismoi
     
 def findSeismoiPoints():
@@ -83,21 +88,53 @@ def findSeismoiPoints():
     
     seismoi = openSeismoiFile()
     print("Latitude, Longitude, Magnitude, Month, Year, Perifereia")
+    counter = 1
     for index, multipolygon in enumerate(multipolygons):
-        for seismos in seismoi:
-            if sg.Point(seismos.lon, seismos.lat).intersects(multipolygon):
-                _tmp = [str(item).rstrip() for item in seismos.prnt()]
-                if int(_tmp[4]) >= 1960 and int(_tmp[4]) <= 2010:
-                    print(f"{_tmp[0]}, {_tmp[1]}, {_tmp[2]}, {get_month(_tmp[3])}, {_tmp[4]}, {regions[index]}")
-                #print(multipolygon)
-                #print()
+        string = "Latitude,Longitude,month,year,Magnitude\n"
+        if index in DESIRED_INDICES:
+            for seismos in seismoi:
+                if sg.Point(seismos.lon, seismos.lat).intersects(multipolygon):
+                    _tmp = [str(item).rstrip() for item in seismos.prnt()]
+                    if int(_tmp[4]) >= 1960 and int(_tmp[4]) <= 2010:
+                        print(f"{_tmp[0]}, {_tmp[1]}, {_tmp[2]}, {get_month(_tmp[3])}, {_tmp[4]}, {regions[index]}")
+                        string += f"{_tmp[0]}, {_tmp[1]}, {get_month(_tmp[3])}, {_tmp[4]}, {_tmp[2]}\n"
+            with open(f"{OUTPUT_PATH}output{counter}.csv", 'w') as f:
+                f.write(string)
+                counter += 1
+                    
+def visualizeData():
+    earthquake_counts = {}
+    region_names = ['Thessaly', 'Attica']  # Add more region names as needed
+
+    for file in glob.glob(f"{OUTPUT_PATH}output*.csv"):
+        with open(file, 'r') as f:
+            reader = csv.DictReader(f)
+            decades = {}
+            for row in reader:
+                year = int(row['year'])
+                decade = round(year / 10) * 10  # Round to the nearest decade
+                if decade not in decades:
+                    decades[decade] = 0
+                decades[decade] += 1
+            file_number = int(file.split('\\')[-1].split('output')[1].split('.csv')[0]) -1 #auto doulevei sta windows, den exw idea giati to file einai output\\output{index}.csv
+            # an petaei error sthn eksetash einai epeidh diaforetika environments kanoun parse to path diaforetika
+            region_name = region_names[file_number]  # file_number -> index -> array index -> name
+            earthquake_counts[region_name] = decades
+
+    for region, counts in earthquake_counts.items():
+        plt.figure()
+        plt.bar(counts.keys(), counts.values())
+        plt.title(f'Number of earthquakes per decade in {region}')
+        plt.xlabel('Decade')
+        plt.ylabel('Number of earthquakes')
+        plt.show()        
 
 
 def main():
     # ftiaxnoume ta csv files
     serializePerifereies(loadPerifereies())
-    
     findSeismoiPoints()
+    visualizeData()
     
     
 
